bb1166474e8a2a24071a6ba27808c13b42679ea0
locobuzz-ganesh <saipavan.ganesh@locobuzz.com>
2025-04-11 23:25:25 +0530

    login api, accpeted cookies using {withCredentials: true}

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
+    const [emailId, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const handleLogin = async () => {
+        try {
+            const res = await axios.post("http://localhost:7777/login", {
+                emailId,
+                password
+            },{withCredentials: true});
+        }
+        catch (err) {
+            console.error(err);
+        }
+    }
+    return (
+        <div className='flex justify-center my-10'>
+            <div className="card bg-base-300 w-96 shadow-sm">
+                <div className="card-body">
+                    <h2 className="card-title justify-center">Login</h2>
+                    <div>
+                        <label className="input my-2">
+                            <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2.5" fill="none" stroke="currentColor"><path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></g></svg>
+                            <input type="input" required placeholder="Email" value={emailId} onChange={(e) => setEmail(e.target.value)} />
+                        </label>
+                    </div>
+                    <div>
+                        <label className="input my-2">
+                            <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2.5" fill="none" stroke="currentColor"><path d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z"></path><circle cx="16.5" cy="7.5" r=".5" fill="currentColor"></circle></g></svg>
+                            <input type="password" required placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                        </label>
+                    </div>
+                    <div className="card-actions justify-center m-2">
+                        <button className="btn btn-primary" onClick={handleLogin}>Login</button>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
 }
 
 export default Login
