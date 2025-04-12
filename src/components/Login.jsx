c0ff671524bf9fcabc2f3fffc97c1fdbe473770b
locobuzz-ganesh <saipavan.ganesh@locobuzz.com>
2025-04-12 17:48:08 +0530

    Redux implementation for login and refactored the code.

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,15 +1,26 @@
 import axios from 'axios';
 import React, { useState } from 'react'
+import { useDispatch } from 'react-redux';
+import { addUser } from '../utils/userSlice';
+import { useNavigate } from 'react-router-dom';
+import { BASE_URL } from '../utils/constants';
 
 const Login = () => {
     const [emailId, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const dispatch = useDispatch();
+    const Navigate = useNavigate();
+
+
     const handleLogin = async () => {
         try {
-            const res = await axios.post("http://localhost:7777/login", {
+            const res = await axios.post(BASE_URL + "/login", {
                 emailId,
                 password
-            },{withCredentials: true});
+            }, { withCredentials: true });
+            console.log(res.data);
+            dispatch(addUser(res.data));
+            return Navigate("/");
         }
         catch (err) {
             console.error(err);
